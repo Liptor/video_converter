8899f7d47d6d283826af2ef89ceb791564eb2c76
ivaningov <i.ingovatov@vibelab.ru>
2025-02-18 14:26:30 +0300

    fix router

--- src/routes/video.ts
+++ src/routes/video.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
 import { getVideo, uploadVideo } from '../controllers/video';
+import upload from '../middlewares/multerMiddleware';
 
 export const router = Router();
 
-router.post('/upload', uploadVideo);
+router.post('/upload', upload.single('file'), uploadVideo);
 
 router.get('/download/:filename', getVideo);
